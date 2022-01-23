eb4ce6488a4515c62c7dce8aace1438ce5523be8
Tobias Thomas <tobias.thomas@stud.tu-darmstadt.de>
2022-01-23 17:51:56 +0100

    add first page content

---
+++ blogposts/index.md
@@ -0,0 +1,5 @@
+@def title = "Blog"
+@def pubdate = "2022-1-1"
+
+# Blog
+{{blogposts}}
--- utils.jl
+++ utils.jl
@@ -8,6 +8,27 @@ function hfun_m1fill(vname)
   return pagevar("index", var)
 end
 
+function hfun_blogposts()
+    bloglist = readdir("blogposts")
+    filter!(f -> endswith(f, ".md"), bloglist)
+    sorter(p) = Date(pagevar("blogposts/$p", "pubdate"), dateformat"y-m-d")
+    sort!(bloglist, by=sorter, rev=true)
+
+    io = IOBuffer()
+    write(io, """<ul class="blog-posts">""")
+    for (i, post) in enumerate(bloglist)
+        post == "index.md" && continue
+        write(io, "<li><span><i>")
+        url = "blogposts/$(post[1:end-3])"
+        title = pagevar(url, "longtitle")
+        pubdate = pagevar(url, "pubdate")
+        date = Date(pubdate, dateformat"y-m-d")
+        write(io, """$date</i></span><a href="/$url/">$title</a>""")
+    end
+    write(io, "</ul>")
+    return String(take!(io))
+end
+
 function lx_baz(com, _)
   # keep this first line
   brace_content = Franklin.content(com.braces[1]) # input string
